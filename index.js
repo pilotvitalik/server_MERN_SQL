4681769fe96aba46b37a309bdabb39ce402b1443
Vitaliy <itvkip@yandex.ru>
2020-12-03 17:47:35 +0300

    Created first connection with DB

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
 const app = express();
+
+const db = mysql.createConnection({
+	user: 'root',
+	host: 'localhost',
+	password: '12345678!',
+	database: 'employeeSystem'	
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/create', (req, res) => {
+	console.log(req.body)
+	const name = req.body.name;
+	const age = req.body.age;
+	const country = req.body.country;
+	const position = req.body.position;
+	const wage = req.body.wage;
+
+	db.query('INSERT INTO employees (name, age, country, position, wage) VALUES (?, ?, ?, ?, ?)',
+		[ name, age, country, position, wage ],
+		(err, result) => {
+			if (err){
+				console.log(err);
+			} else {
+				console.log('values inserted');
+			}
+	} );
+});
 
 app.listen(3001, () => {
 	console.log('Server start on port 3001');
